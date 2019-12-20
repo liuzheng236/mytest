a38a41345397eabb038884edce94d64d3db5003e
zheng <123@163.com>
2019-12-20 09:57:46 +0800

    first commit

--- src/main/java/com/itheima/HelloWorld.java
+++ src/main/java/com/itheima/HelloWorld.java
@@ -2,6 +2,6 @@ package com.itheima;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("nihao");
     }
 }
